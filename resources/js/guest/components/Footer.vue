3e74325768a7b2bd9b9f0ac5cb963513476bd339
Roberto D'Ambrosio <roberto8601@gmail.com>
2022-02-24 15:49:18 +0100

    first commit

---
+++ resources/js/guest/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+      <h2>Ciao sono il footer piu brutto di sempre</h2>
+  </footer>
+</template>
+
+<script>
+export default {
+ name: 'Footer'
+}
+</script>
---
+++ resources/js/guest/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+      <h1>Ciao sono l'header piu brutto di sempre</h1>
+  </header>
+</template>
+
+<script>
+export default {
+ name: 'Header'
+}
+</script>
+
+
